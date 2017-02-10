7800517dc04b35e3c908dcb0ee562b27ad42d853
Le0Michine <leomichine@gmail.com>
2017-02-10 17:37:34 +0100

    init

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # webpack-string-replacer-plugin
-Replaces strings in assets according to provided regex or string
+
+A plugin for replacing string value in assets.
+
+Installation
+
+```
+npm install --save-dev webpack-string-replacer-plugin
+```
+
+Usage
+```javascript
+const StringReplacerPlugin = require('webpack-string-replacer-plugin');
+
+...
+
+// add to webpack plugins array
+plugins: [
+  new StringReplacerPlugin(options)
+]
+
+/*
+  options: {
+    files: array assets names to update, by default all assets will be updated
+    replaceValue: regex or string to replace, this option is mandatory
+    newValue: new value to insert, by default is ''
+  }
+*/
+```
---
+++ index.js
@@ -0,0 +1,37 @@
+function StringReplacerPlugin(options) {
+    const { files, replaceValue, newValue } = options || {};
+    this.files = files;
+    this.replaceValue = replaceValue;
+    this.newValue = newValue || '';
+    if (!replace) {
+        throw new Error('value to replace should be provided');
+    }
+}
+
+StringReplacerPlugin.prototype.apply = function(compiler) {
+    const files = this.files;
+    const replaceValue = this.replaceValue;
+    const newValue = this.newValue;
+
+    compiler.plugin("emit", function(compilation, callback) {
+        var assetNames = getAssetsToProcess(compilation.assets, files);
+        assetNames.forEach(name => {
+            var asset = compilation.assets[name];
+            var content = asset.source();
+            if (content) {
+                const updatedContent = content.replace(replaceValue, newValue);
+                compilation.assets[name] = { source: () => updatedContent, size: () => updatedContent.length };
+            } else {
+                console.log(`\nunable to read asset ${name}, probably it is not a text file`);
+            }
+            callback();
+        });
+    });
+};
+
+function getAssetsToProcess(assets, files) {
+    const assetNames = Object.keys(assets).filter(x => files ? files.some(f => x.match(f)) : true);
+    return assetNames;
+}
+
+module.exports = StringReplacerPlugin;
